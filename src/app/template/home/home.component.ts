98588eb3fc9cf77170f23098b0fb4ebebf74b4bb
gfferreiira <arzhun7381@gmail.com>
2024-04-04 22:20:53 -0300

    Explicação e exemplos de como funciona o TypeSricpt (variáveis, funções)

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    nomeProduto : string = "Curso de Angular";
+    anuncio: string = `O ${this.nomeProduto} está em promoção`;
+    idProduto: number = 123;
+    precoProduto : number = 2.59;
+    promocao: boolean = true;
 
+  constructor() {
+      // variáveis de string com concatenação
+      // this.anuncio = "O"  + this.nomeProduto + "está em promoção";
+      console.log("Nome do Produto: ", this.nomeProduto);
+      console.log("Anuncio: ", this.anuncio);
+      console.log("ID: ", this.idProduto);
+      console.log("Preço: ", this.precoProduto);
+      console.log("Promoção: ", this.promocao);
+
+      //Escopo das variáveis dentro do código
+      var variavel1;
+      let variavel2;
+     const variavel3 = 100;
+
+     var idade = 10
+     console.log("Minha idade é " + idade)
+
+     /* function imprimeIdade() {
+      var idade = 50
+      console.log("Minha idade é " + idade)
+     }
+     imprimeIdade() */
+
+     function imprimeIdade()
+     {
+      for (let idade  = 30; idade <=40; idade ++){
+        console.log("Idade dentro do for: " + idade)
+      }
+      console.log("Idade fora do for " + idade)
+     }
+     imprimeIdade()
+  }
 }
