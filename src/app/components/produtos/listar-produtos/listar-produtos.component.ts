a33b5fc9693fe896081c18bfa30d2f20b1ca08da
gfferreiira <arzhun7381@gmail.com>
2024-04-25 22:01:12 -0300

    Adição da aba cadastrar produtos

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-produtos',
   templateUrl: './listar-produtos.component.html',
   styleUrls: ['./listar-produtos.component.css']
 })
-export class ListarProdutosComponent {
+export class ListarProdutosComponent implements OnInit {
+
+  listaString: string [] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
+
+  objetoModelo = {
+    nome: 'Fatima A MELHOR',
+    idade: 18,
+    altura: 1.56,
+    graduado: true
+  };
+
+  listaProdutos: any[] = [
+      {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
+      {nome: 'Curso de Ionic' , precoProduto: 50, validade: '2021-12-31', id: 2},
+      {id: 3, nome: 'Curso de Ionic Avançado', precoProduto:50, validade: '2021-12-31'}
+
+  ]
+
+  constructor() {
+
+    for (let item of this.listaString){
+      console.log(item);
+    }
+
+    for(const item of this.listaNumeros){
+      console.log(item);
+    }
+
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome)
+
+  }
+
+
+    ngOnInit(): void{ }
 
 }
